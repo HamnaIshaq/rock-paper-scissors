c975df6e211a4e23a3e0a36154eefade8b8e2e8f
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-09-14 19:10:56 +0500

    Play one round between player and computer

--- app.js
+++ app.js
@@ -15,3 +15,42 @@ function getComputerChoice() {
       return 'scissors'
   }
 }
+
+// play round of rock, paper, scissors between player and computer
+function playRound(playerSelection, computerSelection) {
+  // 
+  const playerChoice = playerSelection.toLowerCase();
+  
+  const computerChoice = computerSelection.toLowerCase();
+  
+  if(playerChoice === computerChoice) {
+    return 'Both choose '+ playerChoice + '. It is a draw'
+  }
+  else if(playerChoice === 'rock' && computerChoice === 'scissors') {
+    return 'You Win! Rock beats Scissors'  
+  }
+  else if(playerChoice === 'rock' && computerChoice === 'paper') {
+    return 'You Lose! Paper beats Rock'  
+  }
+  else if(playerChoice === 'paper' && computerChoice === 'scissors') {
+    return 'You Lose! Scissors beat Paper'  
+  }
+  else if(playerChoice === 'paper' && computerChoice === 'rock') {
+    return 'You Win! Paper beats Rock'  
+  }
+  else if(playerChoice === 'scissors' && computerChoice === 'rock') {
+    return 'You Lose! Rock beats Scissors'  
+  }
+  else if(playerChoice === 'scissors' && computerChoice === 'paper') {
+    return 'You Win! Scissors beat Paper'  
+  }
+}
+
+// get player choice (hard coded value for testing)
+const playerSelection = 'scissors';
+
+// get computer choice
+const computerSelection = getComputerChoice();
+
+// play rock, paper, scissors round
+console.log(playRound(playerSelection, computerSelection))
