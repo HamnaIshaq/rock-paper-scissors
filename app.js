2e4481f4f60d8d1dd9b1c42a13e5d64117790bea
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-09-14 14:19:37 +0500

    Get random computer choice for rock, paper, scissors

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('connected to HTML')
+
+// get computer choice
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 3);
+  switch(computerChoice) {
+    case 0:
+      return 'Computer Chooses: Rock'
+    case 1:
+      return 'Computer Chooses: Paper'
+    case 2:
+      return 'Computer Chooses: Scissors'
+  }
+}
